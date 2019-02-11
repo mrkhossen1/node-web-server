b19ec305f91a1a806cd4b1e6a7f5a3df22cd62b1
Kamal Hossen <mrkhossen@outlook.com>
2019-02-11 11:29:27 +1030

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app= express();
+
+var port=3000;
+// Use partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+// use helpers
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+// Set hbs scope.....
+app.set('view engine','hbs')
+
+//Use of static page in Node JS
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
+  var now = new Date();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + `\n`,(err) => {
+    if(err){
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// Maintenance Page
+//app.use((req,res,next) => {
+//  res.render('maintenance.hbs')
+//});
+
+app.get('/',(req,res) => {
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to the page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About us',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage: 'Unable to handle the request',
+    status:[
+      400,
+      'Not OK'
+    ]
+  });
+});
+
+app.listen(port,() => console.log(`Server is running on port ${port}`));
